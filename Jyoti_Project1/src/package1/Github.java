b62211b33e3fa7db6973a62bc490913810e59aad
hp <hp@DESKTOP-D2K8MVD>
2024-02-10 23:32:56 +0530

    second time code push

---
+++ Jyoti_Project1/src/package1/Github.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class Github {
+
+	void method1()
+	{
+		System.out.println("Code is pushed into github");
+	}
+	public static void main(String args[])
+	{
+		Github obj9=new Github();
+		obj9.method1();
+	}
+}
